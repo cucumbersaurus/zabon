5947072eba14f5e20174dc99e39b3600019895ff
Cucumbersaurus <cucumbersaurus@gmail.com>
2022-01-03 11:16:47 +0900

    ... lol

--- src/main/java/cucumbersaurus/zabon/joinQuitLoger/QuitLogger.java
+++ src/main/java/cucumbersaurus/zabon/joinQuitLoger/QuitLogger.java
@@ -1,8 +1,6 @@
 package cucumbersaurus.zabon.joinQuitLoger;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -27,21 +25,51 @@ public class QuitLogger {
 
     public void quitCucumber(PlayerQuitEvent e){
         e.setQuitMessage(ChatColor.YELLOW+"위대한 "+ChatColor.GREEN+"오이사우르스"+ChatColor.YELLOW+"가 바람과 함께 사라졌습니다.");
+
+        World world =  e.getPlayer().getWorld();
+        Location location = e.getPlayer().getLocation();
+
+        if(world.getBlockAt(location).getType()==Material.LAVA){
+            e.setQuitMessage(ChatColor.GREEN+"오이사우르스"+ChatColor.YELLOW+"가 용암에 빠진 것 같습니다.");
+        }
         alertQuit();
     }
 
     public void quitAndesite(PlayerQuitEvent e){
         e.setQuitMessage(ChatColor.GOLD+"데스"+ChatColor.YELLOW+"가 \"데스는 데스데스\"를 시전하며 퇴장했습니다.");
+
+        World world =  e.getPlayer().getWorld();
+        Location location = e.getPlayer().getLocation();
+
+        if(world.getBlockAt(location).getType()==Material.LAVA){
+            e.setQuitMessage(ChatColor.GOLD+"데스"+ChatColor.YELLOW+"가 용암에 빠진 것 같습니다.");
+        }
+
         alertQuit();
     }
 
     public void quitSeo_m(PlayerQuitEvent e){
         e.setQuitMessage(ChatColor.YELLOW+"마이크 귀찮아서 안들고온 "+ChatColor.AQUA+"서엠"+ChatColor.YELLOW+"이 서버에서 퇴장하였습니다.");
+
+        World world =  e.getPlayer().getWorld();
+        Location location = e.getPlayer().getLocation();
+
+        if(world.getBlockAt(location).getType()==Material.LAVA){
+            e.setQuitMessage(ChatColor.AQUA+"서엠"+ChatColor.YELLOW+"이 용암에 빠진 것 같습니다.");
+        }
+
         alertQuit();
     }
 
     public void quitDefault(PlayerQuitEvent e){
         e.setQuitMessage(ChatColor.DARK_GREEN+e.getPlayer().getName()+ChatColor.YELLOW+"(이)가 탈주했습니다!");
+
+        World world =  e.getPlayer().getWorld();
+        Location location = e.getPlayer().getLocation();
+
+        if(world.getBlockAt(location).getType()==Material.LAVA){
+            e.setQuitMessage(ChatColor.DARK_GREEN+e.getPlayer().getName()+ChatColor.YELLOW+"가 용암에 빠진 것 같습니다.");
+        }
 
         alertQuit();
     }
